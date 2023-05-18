fea5dfa03db4e6b7e328d55554019040fcf5ab97
lilsin991020 <lil@superchart.com>
2023-05-18 15:07:23 +0900

    feat: s3이미지 업로드 init

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,24 @@
-import { Controller, Get, Post, Body, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { Response } from 'express';
+import { Express, Response } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('user')
 export class UserController {
   constructor(private addressService: UserService) {}
+
+  @Post('profile')
+  @UseInterceptors(FileInterceptor('profileImage'))
+  createProfile(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    console.log(file);
+    console.log(body);
+  }
 }
