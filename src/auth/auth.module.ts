acfcdced4e03ca70fccfa5f0dff07271eb18456c
lilsin991020 <lil@superchart.com>
2023-05-22 15:25:56 +0900

    feat: jwtToken init

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User, UserSchema } from '../schema/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../constants/constants';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../schema/user.schema';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
