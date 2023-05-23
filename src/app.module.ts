eb61014b1a087b1399eb093e2320553172009007
sinyeongseok <sinyeongseok99@gmail.com>
2023-05-23 23:43:49 +0900

    feat: jwt token 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { WebModule } from './web/web.module';
+import { JwtModule } from '@nestjs/jwt';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
+      load: [configuration],
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
     }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
     AddressModule,
