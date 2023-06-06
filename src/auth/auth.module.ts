6ec89354d074defbe4165b8131b06d20924495df
sinyeongseok <sinyeongseok99@gmail.com>
2023-06-07 01:49:28 +0900

    style: 불필요한 콘솔로그 제거

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User, UserSchema } from '../schema/user.schema';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
