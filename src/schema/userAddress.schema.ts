fb7f231598baabd9d666a306d3df2d7aa0ebc110
sinyeongseok <sinyeongseok99@gmail.com>
2023-06-12 22:21:38 +0900

    feat: userAddress 스키마 추가

---
+++ src/schema/userAddress.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserAddressDocument = UserAddress & Document;
+
+@Schema({ timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
+export class UserAddress {
+  @Prop()
+  userEmail: string;
+
+  @Prop()
+  detail: string;
+
+  @Prop()
+  longitude: string;
+
+  @Prop()
+  latitude: string;
+
+  @Prop()
+  isLastSelected: boolean;
+
+  @Prop()
+  isAuth: boolean;
+}
+
+export const UserAddressSchema = SchemaFactory.createForClass(UserAddress);
