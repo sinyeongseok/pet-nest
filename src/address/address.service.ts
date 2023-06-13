d713d5275163a1d9160ff959cb8763b5f689bcec
sinyeongseok <sinyeongseok99@gmail.com>
2023-06-13 21:43:25 +0900

    refactor: 중복되는 코드 함수로 빼기

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -8,6 +8,26 @@ export class AddressService {
   constructor(
     @InjectModel(Address.name) private AddressModel: Model<AddressDocument>
   ) {}
+
+  formatAddressDetail({ siDo, eupMyeonDong, siGunGu = '', ri = '' }) {
+    return [siDo, siGunGu, eupMyeonDong, ri]
+      .join(' ')
+      .replaceAll('  ', ' ')
+      .trim();
+  }
+
+  formatAddressList(addressList: AddressDocument[]) {
+    return addressList.map(({ coordinate, ...addressInfo }) => {
+      const address = this.formatAddressDetail(addressInfo);
+      const [latitude, longitude] = coordinate.split(',');
+      return {
+        address,
+        latitude,
+        longitude,
+      };
+    });
+  }
+
   async getNearbyAddresses(latitude: string, longitude: string) {
     const getDistance = (
       lat1: number,
@@ -15,13 +35,10 @@ export class AddressService {
       lat2: number,
       lon2: number
     ) => {
-      if (lat1 == lat2 && lon1 == lon2) {
-        return 0;
-      }
-      let radLat1 = (Math.PI * lat1) / 180;
-      let radLat2 = (Math.PI * lat2) / 180;
-      let theta = lon1 - lon2;
-      let radTheta = (Math.PI * theta) / 180;
+      const radLat1 = (Math.PI * lat1) / 180;
+      const radLat2 = (Math.PI * lat2) / 180;
+      const theta = lon1 - lon2;
+      const radTheta = (Math.PI * theta) / 180;
       let dist =
         Math.sin(radLat1) * Math.sin(radLat2) +
         Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radTheta);
@@ -32,11 +49,10 @@ export class AddressService {
       dist = (dist * 180) / Math.PI;
       dist = dist * 60 * 1.1515 * 1.609344 * 1000;
       if (dist < 100) {
-        dist = Math.round(dist / 10) * 10;
+        return Math.round(dist / 10) * 10;
       } else {
-        dist = Math.round(dist / 100) * 100;
+        return Math.round(dist / 100) * 100;
       }
-      return dist;
     };
 
     try {
@@ -56,21 +72,11 @@ export class AddressService {
         }
       });
 
-      const result = nearbyAddressList
-        .sort((a, b) => a.distance - b.distance)
-        .map((addressInfo) => {
-          const address = `${addressInfo['siDo']}${
-            !!addressInfo['siGunGu'] ? ` ${addressInfo['siGunGu']}` : ''
-          } ${addressInfo['eupMyeonDong']} ${
-            !!addressInfo['ri'] ? addressInfo['ri'] : ''
-          }`.trim();
-          const [latitude, longitude] = addressInfo['coordinate'].split(',');
-          return {
-            address,
-            latitude,
-            longitude,
-          };
-        });
+      const sortArray = nearbyAddressList.sort(
+        (a, b) => a.distance - b.distance
+      );
+      const result = this.formatAddressList(sortArray);
+
       return result;
     } catch (error) {
       console.log(error);
@@ -83,17 +89,8 @@ export class AddressService {
       const addressList = await this.AddressModel.find({
         ...(!!address && { eupMyeonDong: { $regex: regExp } }),
       }).lean();
+      const result = this.formatAddressList(addressList);
 
-      const result = addressList.map((addressInfo) => {
-        const address = `${addressInfo['siDo']}${
-          !!addressInfo['siGunGu'] ? ` ${addressInfo['siGunGu']}` : ''
-        } ${addressInfo['eupMyeonDong']} ${
-          !!addressInfo['ri'] ? addressInfo['ri'] : ''
-        }`.trim();
-        const [latitude, longitude] = addressInfo['coordinate'].split(',');
-
-        return { address, latitude, longitude };
-      });
       return result;
     } catch (error) {
       console.log(error);
