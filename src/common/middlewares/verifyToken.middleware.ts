c2964d0c07a1c88cc22bcf5a40b3bf8bfc6e5450
sinyeongseok <sinyeongseok99@gmail.com>
2023-06-19 21:38:12 +0900

    refactor: 에러코드 수정

--- src/common/middlewares/verifyToken.middleware.ts
+++ src/common/middlewares/verifyToken.middleware.ts
@@ -17,7 +17,7 @@ export class VerifyTokenMiddleware implements NestMiddleware {
         next();
       } catch (error) {
         if (error.message === 'jwt expired') {
-          return res.status(419).json({ message: '만료된 토큰입니다.' });
+          return res.status(401).json({ message: '만료된 토큰입니다.' });
         }
 
         return res.status(401).json({ message: '유효하지 않은 토큰입니다.' });
