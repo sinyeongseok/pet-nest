479217e9c230b51f1157766a09503843ab56baa2
sinyeongseok <sinyeongseok99@gmail.com>
2023-06-29 00:51:05 +0900

    refactor: AWS SDK for JavaScript (v2)에서 AWS SDK for JavaScript (v3)를 사용하도록 코드 변경
    - @aws-sdk/client-s3 모듈 추가

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,17 +1,24 @@
-import * as AWS from 'aws-sdk';
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+  PutObjectCommandOutput,
+  DeleteObjectCommandOutput,
+} from '@aws-sdk/client-s3';
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { PromiseResult } from 'aws-sdk/lib/request';
 
 @Injectable()
 export class AwsService {
-  private readonly awsS3: AWS.S3;
+  private readonly awsS3: S3Client;
   public readonly S3_BUCKET_NAME: string;
 
   constructor() {
-    this.awsS3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    this.awsS3 = new S3Client({
       region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
     });
     this.S3_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
   }
@@ -21,44 +28,39 @@ export class AwsService {
     file: Express.Multer.File
   ): Promise<{
     key: string;
-    s3Object: PromiseResult<AWS.S3.PutObjectOutput, AWS.AWSError>;
+    s3Object: PutObjectCommandOutput;
     contentType: string;
     url: string;
   }> {
     try {
-      const s3Object = await this.awsS3
-        .putObject({
-          Bucket: this.S3_BUCKET_NAME,
-          Key: key,
-          Body: file.buffer,
-          ACL: 'public-read',
-          ContentType: file.mimetype,
-        })
-        .promise();
+      const command = new PutObjectCommand({
+        Bucket: this.S3_BUCKET_NAME,
+        Key: key,
+        Body: file.buffer,
+        ACL: 'public-read',
+        ContentType: file.mimetype,
+      });
+      const s3Object = await this.awsS3.send(command);
       const imgUrl = `https://${this.S3_BUCKET_NAME}.s3.amazonaws.com/${key}`;
       return { key, s3Object, contentType: file.mimetype, url: imgUrl };
     } catch (error) {
-      throw new BadRequestException(`File upload failed : ${error}`);
+      throw new BadRequestException(`File upload failed: ${error.message}`);
     }
   }
 
   async deleteS3Object(
     key: string,
-    callback?: (err: AWS.AWSError, data: AWS.S3.DeleteObjectOutput) => void
+    callback?: (err: Error, data: DeleteObjectCommandOutput) => void
   ): Promise<{ success: true }> {
     try {
-      await this.awsS3
-        .deleteObject(
-          {
-            Bucket: this.S3_BUCKET_NAME,
-            Key: key,
-          },
-          callback
-        )
-        .promise();
+      const command = new DeleteObjectCommand({
+        Bucket: this.S3_BUCKET_NAME,
+        Key: key,
+      });
+      await this.awsS3.send(command);
       return { success: true };
     } catch (error) {
-      throw new BadRequestException(`Failed to delete file : ${error}`);
+      throw new BadRequestException(`Failed to delete file: ${error.message}`);
     }
   }
 
