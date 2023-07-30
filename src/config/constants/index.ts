7ab9a41a34a7ee4c50e17542494645ce3c55c5a7
sinyeongseok <sinyeongseok99@gmail.com>
2023-07-30 18:17:15 +0900

    feat: 시간과 관련된 상수 추가

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,1 +1,9 @@
 export const DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+
+export const ONE_MINUTE = 1000 * 60;
+export const ONE_HOUR = ONE_MINUTE * 60;
+export const DAY_HOURS = ONE_HOUR * 24;
+export const ONE_WEEK = DAY_HOURS * 7;
+export const TWO_WEEK = ONE_WEEK * 2;
+export const ONE_MONTH = 1;
+export const ONE_YEAR = 1;
