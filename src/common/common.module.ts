ef68420c68c9dcb6ee355ac9288a2e9658501441
sinyeongseok <sinyeongseok99@gmail.com>
2023-08-20 21:42:56 +0900

    feat: jwtRefreshStrategy 등록

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JwtAccessStrategy } from './strategy/jwtAccess.strategy';
+import { JwtRefreshStrategy } from './strategy/jwtRefresh.strategy';
 
 @Module({
-  providers: [JwtAccessStrategy],
-  exports: [JwtAccessStrategy],
+  providers: [JwtAccessStrategy, JwtRefreshStrategy],
+  exports: [JwtAccessStrategy, JwtRefreshStrategy],
 })
 export class CommonModule {}
