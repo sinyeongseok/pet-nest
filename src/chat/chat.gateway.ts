c3d37a3b840d288562c602c136ef38761a1320ca
sinyeongseok <sinyeongseok99@gmail.com>
2023-08-22 22:35:44 +0900

    feat: 채팅방 생성 추가

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -19,4 +19,24 @@ import { JwtAccessAuthGuard } from 'src/common/guards/jwtAccessAuthGuard.guard';
 export class ChatGateway {
   constructor(private readonly chatService: ChatService) {}
   @WebSocketServer() nsp: Namespace;
+
+  @SubscribeMessage('create-room-used-item')
+  @UseGuards(JwtAccessAuthGuard)
+  async handleCreateRoom(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() { sellerEmail, usedItemBoardId },
+    @Req() req
+  ) {
+    const email = req.user.email;
+
+    const createChatRoomResult = await this.chatService.createChatRoom(
+      [email, sellerEmail],
+      usedItemBoardId
+    );
+
+    socket.join(createChatRoomResult.id);
+    this.nsp.emit('create-room', createChatRoomResult.id);
+
+    return { success: true, data: { chatRoomId: createChatRoomResult.id } };
+  }
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UsedItemBoard, UsedItemBoardDocument } from 'src/schema/board.schema';
+import { ChatRoom, ChatRoomDocument } from 'src/schema/chatRoom.schema';
+import {
+  ChatRoomSetting,
+  ChatRoomSettingDocument,
+} from 'src/schema/chatRoomSetting.schema';
+import { User, UserDocument } from 'src/schema/user.schema';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @InjectModel(UsedItemBoard.name)
+    private usedItemBoardModel: Model<UsedItemBoardDocument>,
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+    @InjectModel(ChatRoom.name)
+    private chatRoomModel: Model<ChatRoomDocument>,
+    @InjectModel(ChatRoomSetting.name)
+    private chatRoomSettingModel: Model<ChatRoomSettingDocument>
+  ) {}
+
+  async createChatRoom(users, usedItemBoardId) {
+    try {
+      const createChatRoomQuery = new this.chatRoomModel({
+        users,
+        boardId: usedItemBoardId,
+      });
+      const createChatRoomResult = await createChatRoomQuery.save();
+      for await (const user of users) {
+        const createChatRoomSettingQuery = new this.chatRoomSettingModel({
+          chatRoomId: createChatRoomResult._id,
+          userId: user,
+        });
+        await createChatRoomSettingQuery.save();
+      }
+
+      return { id: createChatRoomResult._id };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        '서버요청 실패.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+}
