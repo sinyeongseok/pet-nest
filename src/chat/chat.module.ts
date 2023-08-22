4323c563ba9bce1b8fdf9284e747785bbf9a4846
sinyeongseok <sinyeongseok99@gmail.com>
2023-08-22 22:36:03 +0900

    feat: 채팅방 DB 스키마 연결

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsedItemBoard, UsedItemBoardSchema } from 'src/schema/board.schema';
+import { User, UserSchema } from 'src/schema/user.schema';
+import { UserAddress, UserAddressSchema } from 'src/schema/userAddress.schema';
+import { ChatRoom, ChatRoomSchema } from 'src/schema/chatRoom.schema';
+import {
+  ChatRoomSetting,
+  ChatRoomSettingSchema,
+} from 'src/schema/chatRoomSetting.schema';
+import { Message, MessageSchema } from 'src/schema/message.schema';
 
 @Module({
-  providers: [ChatGateway, ChatService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: UsedItemBoard.name, schema: UsedItemBoardSchema },
+      { name: UserAddress.name, schema: UserAddressSchema },
+      { name: User.name, schema: UserSchema },
+      { name: ChatRoom.name, schema: ChatRoomSchema },
+      { name: ChatRoomSetting.name, schema: ChatRoomSettingSchema },
+      { name: Message.name, schema: MessageSchema },
+    ]),
+  ],
+  providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
