fdb7080b78dc1c783550108ce71008d45fcf58b5
sinyeongseok <sinyeongseok99@gmail.com>
2023-10-06 13:32:24 +0900

    chore: User, UserAddress schema 등록

--- src/my-page/my-page.module.ts
+++ src/my-page/my-page.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MyPageController } from './my-page.controller';
 import { MyPageService } from './my-page.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schema/user.schema';
+import { UserAddress, UserAddressSchema } from 'src/schema/userAddress.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: UserAddress.name, schema: UserAddressSchema },
+    ]),
+  ],
   controllers: [MyPageController],
-  providers: [MyPageService]
+  providers: [MyPageService],
 })
 export class MyPageModule {}
