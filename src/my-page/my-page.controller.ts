bd7fa37b4bd40d57bf71eaca1e66941a9014100f
sinyeongseok <sinyeongseok99@gmail.com>
2023-10-06 13:32:49 +0900

    feat: 마이페이지 유저정보 가져오기 api 추가

--- src/my-page/my-page.controller.ts
+++ src/my-page/my-page.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { JwtAccessAuthGuard } from 'src/common/guards/jwtAccessAuthGuard.guard';
+import { MyPageService } from './my-page.service';
 
 @Controller('my-page')
-export class MyPageController {}
+export class MyPageController {
+  constructor(private myPageService: MyPageService) {}
+
+  @Get('user-info')
+  @UseGuards(JwtAccessAuthGuard)
+  async getMyPageUserInfo(@Req() req, @Res() res) {
+    const email = req.user.email;
+    const result = await this.myPageService.getMyPageUserInfo(email);
+
+    return res.status(result.statusCode).json(result.data);
+  }
+}
--- src/my-page/my-page.service.ts
+++ src/my-page/my-page.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schema/user.schema';
+import {
+  UserAddress,
+  UserAddressDocument,
+} from 'src/schema/userAddress.schema';
 
 @Injectable()
-export class MyPageService {}
+export class MyPageService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+    @InjectModel(UserAddress.name)
+    private userAddressModel: Model<UserAddressDocument>
+  ) {}
+
+  async getMyPageUserInfo(email: string) {
+    try {
+      const getUserInfo = this.userModel.findOne({ email });
+      const getUserAddressInfo = this.userAddressModel.find({
+        userEmail: email,
+      });
+      const [userInfo, userAddressInfo] = (await Promise.all([
+        getUserInfo,
+        getUserAddressInfo,
+      ])) as [UserDocument, UserAddressDocument[]];
+      const userAddress = (() => {
+        if (userAddressInfo.length === 1) {
+          return userAddressInfo[0].eupMyeonDong;
+        }
+
+        return userAddressInfo
+          .map((userAddress) => userAddress.eupMyeonDong)
+          .join(' / ');
+      })();
+      const result = {
+        nickname: userInfo.nickname,
+        address: userAddress,
+        ...(!!userInfo.profileImage && { profileImage: userInfo.profileImage }),
+      };
+
+      return { statusCode: 200, data: { userInfo: result } };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        '서버요청 실패.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+}
