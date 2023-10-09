a5c09a33823361105fce8baf21165016dd5a307a
sinyeongseok <sinyeongseok99@gmail.com>
2023-10-09 20:13:01 +0900

    feat: 펫 추가하기 api 추가

--- src/my-page/my-page.controller.ts
+++ src/my-page/my-page.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { JwtAccessAuthGuard } from 'src/common/guards/jwtAccessAuthGuard.guard';
 import { MyPageService } from './my-page.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('my-page')
 export class MyPageController {
@@ -11,6 +22,21 @@ export class MyPageController {
   async getMyPageUserInfo(@Req() req, @Res() res) {
     const email = req.user.email;
     const result = await this.myPageService.getMyPageUserInfo(email);
+
+    return res.status(result.statusCode).json(result.data);
+  }
+
+  @Post('pet')
+  @UseGuards(JwtAccessAuthGuard)
+  @UseInterceptors(FilesInterceptor('petImages', 5))
+  async createPet(
+    @Res() res,
+    @Req() req,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() petInfo
+  ) {
+    const email = req.user.email;
+    const result = await this.myPageService.createPet(email, files, petInfo);
 
     return res.status(result.statusCode).json(result.data);
   }
--- src/my-page/my-page.service.ts
+++ src/my-page/my-page.service.ts
@@ -1,11 +1,15 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Pet, PetDocument } from 'src/schema/pet.schema';
 import { User, UserDocument } from 'src/schema/user.schema';
 import {
   UserAddress,
   UserAddressDocument,
 } from 'src/schema/userAddress.schema';
+import { AwsService } from 'src/utils/s3';
+import { v4 as uuid } from 'uuid';
+import * as dayjs from 'dayjs';
 
 @Injectable()
 export class MyPageService {
@@ -13,7 +17,10 @@ export class MyPageService {
     @InjectModel(User.name)
     private userModel: Model<UserDocument>,
     @InjectModel(UserAddress.name)
-    private userAddressModel: Model<UserAddressDocument>
+    private userAddressModel: Model<UserAddressDocument>,
+    @InjectModel(Pet.name)
+    private petModel: Model<PetDocument>,
+    private awsService: AwsService
   ) {}
 
   async getMyPageUserInfo(email: string) {
@@ -42,6 +49,55 @@ export class MyPageService {
       };
 
       return { statusCode: 200, data: { userInfo: result } };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        '서버요청 실패.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  async createPet(
+    email,
+    files,
+    {
+      name,
+      species,
+      birthday,
+      gender,
+      neuteredStatus,
+      weight,
+      unusualCondition,
+      helloMessage,
+    }
+  ) {
+    try {
+      const createPetQuery = new this.petModel({
+        name,
+        species,
+        birthday,
+        gender,
+        neuteredStatus,
+        weight,
+        unusualCondition,
+        helloMessage,
+        userEmail: email,
+      });
+      const saveResult = await createPetQuery.save();
+      const imageUploaded = files.map(async (file) => {
+        return await this.awsService.uploadFileToS3(
+          `petImages/${email}/${String(
+            saveResult._id
+          )}/${uuid()}${dayjs().format('YYYYMMDDHHmmss')}`,
+          file
+        );
+      });
+      const imageUploadResults = await Promise.all(imageUploaded);
+      const images = imageUploadResults.map((result) => result.url);
+      await this.petModel.updateOne({ _id: saveResult._id }, { images });
+
+      return { statusCode: 201, data: { isCreated: true } };
     } catch (error) {
       console.log(error);
       throw new HttpException(
