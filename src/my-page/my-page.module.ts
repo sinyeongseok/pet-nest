9b05ec6930e2875e54a09216f0739936377cd496
sinyeongseok <sinyeongseok99@gmail.com>
2023-10-09 20:12:28 +0900

    feat: Pet schema, AwsService 등록

--- src/my-page/my-page.module.ts
+++ src/my-page/my-page.module.ts
@@ -4,15 +4,18 @@ import { MyPageService } from './my-page.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schema/user.schema';
 import { UserAddress, UserAddressSchema } from 'src/schema/userAddress.schema';
+import { Pet, PetSchema } from 'src/schema/pet.schema';
+import { AwsService } from 'src/utils/s3';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: UserAddress.name, schema: UserAddressSchema },
+      { name: Pet.name, schema: PetSchema },
     ]),
   ],
   controllers: [MyPageController],
-  providers: [MyPageService],
+  providers: [MyPageService, AwsService],
 })
 export class MyPageModule {}
