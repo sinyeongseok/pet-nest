076e882e759de1627a1e7d9734ad3d1a259d0b0b
sinyeongseok <sinyeongseok99@gmail.com>
2023-10-12 19:49:33 +0900

    feat: PetGender 추가

--- src/config/type/index.ts
+++ src/config/type/index.ts
@@ -2,7 +2,12 @@ export type AlarmTime = '5분 전' | '10분 전' | '30분 전' | '1시간 전';
 
 export type SalesStatus = '판매중' | '예약중' | '판매완료' | '삭제됨';
 
-export enum petType {
+export enum PetType {
   'dog' = '강아지',
   'cat' = '고양이',
+}
+
+export enum PetGender {
+  'M' = '남아',
+  'F' = '여아',
 }
