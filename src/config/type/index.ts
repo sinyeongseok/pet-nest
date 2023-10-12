f3ba79c312dce16012ad0cbfca8edd370347355d
sinyeongseok <sinyeongseok99@gmail.com>
2023-10-12 19:18:50 +0900

    feat: petType 추가

--- src/config/type/index.ts
+++ src/config/type/index.ts
@@ -1,3 +1,8 @@
 export type AlarmTime = '5분 전' | '10분 전' | '30분 전' | '1시간 전';
 
 export type SalesStatus = '판매중' | '예약중' | '판매완료' | '삭제됨';
+
+export enum petType {
+  'dog' = '강아지',
+  'cat' = '고양이',
+}
