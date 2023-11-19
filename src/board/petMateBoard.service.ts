06b0a182adb6456c3ebed8f4458125ccdaec7279
sinyeongseok <sinyeongseok99@gmail.com>
2023-11-19 17:53:19 +0900

    feat: createPetMateBoard() 추가

--- src/board/petMateBoard.service.ts
+++ src/board/petMateBoard.service.ts
@@ -1,6 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import {
+  participatingPets,
+  participatingPetsDocument,
+} from 'src/schema/participatingPetsSchema.schema';
+import {
+  PetMateBoard,
+  PetMateBoardDocument,
+} from 'src/schema/petMateBoardSchema.schema';
 
 @Injectable()
-export class PetMateBoard {
-  constructor() {}
+export class PetMateBoardService {
+  constructor(
+    @InjectModel(PetMateBoard.name)
+    private petMateBoardModel: Model<PetMateBoardDocument>,
+    @InjectModel(participatingPets.name)
+    private participatingPetsModel: Model<participatingPetsDocument>
+  ) {}
+
+  async createPetMateBoard({
+    host,
+    petIds,
+    title,
+    content,
+    walkDate,
+    place,
+    maxPet,
+  }) {
+    try {
+      const createPetMateBoard = new this.petMateBoardModel({
+        host,
+        title,
+        content,
+        place,
+        maxPet,
+        data: walkDate,
+      });
+      const createPetMateBoardResult = await createPetMateBoard.save();
+      const createParticipatingPets = petIds.map((petId) => {
+        return new this.participatingPetsModel({
+          boardId: createPetMateBoardResult._id,
+          petId: petId,
+        }).save();
+      });
+
+      await Promise.all(createParticipatingPets);
+
+      return { statusCode: 201, data: { isCreated: true } };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        '서버요청 실패.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
 }
