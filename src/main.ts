afee0f969e04f1c58ec98a4e2e85d42511de003b
sinyeongseok <sinyeongseok99@gmail.com>
2023-11-30 19:17:32 +0900

    rename: dayjsPlugin 파일 이름 변경

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dayjs from 'dayjs';
+import dayjsDateFormatPlugin from './utils/plugin/dayjsDateFormatPlugin';
+
+dayjs.extend(dayjsDateFormatPlugin);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
